1e8fd8ae88e78f961da47c333ec7d28b8d7d671a
nehemiah.sam <nehemiah.sam@LP-PF2J02D1.GEO.CORP.HCL.IN>
2021-02-10 11:41:24 -0600

    First commit

---
+++ CollectionTraining/src/com/hcl/demo/CollectionSort.java
@@ -0,0 +1,23 @@
+package com.hcl.demo;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List; 
+
+public class CollectionSort {
+
+	public static void main(String[] args) {
+		List<Object> stringList = new ArrayList<>();
+		stringList.add("bread");
+		stringList.add(5);
+		stringList.add("apple");
+		stringList.add("zebra");
+		stringList.add("cookie");
+		stringList.add("apple");
+	    System.out.println(stringList);
+	    //Collections.sort(stringList);
+	    //System.out.println("sorted list:" + stringList);
+		
+
+	}
+
+}
