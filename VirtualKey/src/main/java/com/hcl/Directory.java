57bdfa878ccc4c09e230dd61da506d90328ffe13
nehemiah.sam <nehemiah.sam@LP-PF2J02D1.GEO.CORP.HCL.IN>
2021-02-23 17:31:00 -0600

    Sprint 2:Commit 1

--- VirtualKey/src/main/java/com/hcl/Directory.java
+++ VirtualKey/src/main/java/com/hcl/Directory.java
@@ -1,5 +1,29 @@
 package com.hcl;
 
+import java.util.ArrayList;
+
 public class Directory {
+	public static ArrayList<String> rootDirectory = new ArrayList<String>();
+
+	public static void addFiletoList(String name) {
+		rootDirectory.add(name);
+		System.out.println(name + " added to directory.");
+
+	}
+
+	public static void deleteFilefromList(String name) {
+		int index = rootDirectory.indexOf(name);
+		rootDirectory.remove(index);
+		System.out.println(name + " deleted from directory.");
+
+	}
+
+	public static void searchFilefromList(String name) {
+		if (rootDirectory.contains(name)) {
+			System.out.println(name + " was found in directory.");
+		} else {
+			System.out.println("The filewas not found in directory.");
+		}
+	}
 
 }
