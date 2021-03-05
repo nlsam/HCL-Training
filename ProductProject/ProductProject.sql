ac4e000fb4864ecadf10947196110a65ba8f4f53
nlsam <78176096+nlsam@users.noreply.github.com>
2021-03-04 20:32:17 -0600

    SQL Script Commit

---
+++ ProductProject/ProductProject.sql
@@ -0,0 +1,16 @@
+create database mydatabase;
+create table fashionstore(
+id numeric (10),
+name varchar (50),
+brand varchar(50),
+price numeric(10),
+primary key (id)
+);
+insert into fashionstore (id, name, brand, price) values(1, Sunglasses, Gucci,350);
+insert into fashionstore (id, name, brand, price) values(2, Scarf, Prada,200);
+insert into fashionstore (id, name, brand, price) values(3, Pants, Prada, 80);
+insert into fashionstore (id, name, brand, price) values(4, Shirt, Versace, 250);
+insert into fashionstore (id, name, brand, price) values(5, Watch, Coach, 250);
+insert into fashionstore (id, name, brand, price) values(6, Belt, Gucci,150);
+insert into fashionstore (id, name, brand, price) values(7, Jacket, Versace, 900);
+
